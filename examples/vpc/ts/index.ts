5c2612cfc489e59274a942800e4d0d3084e89942
stack72 <public@paulstack.co.uk>
2020-03-21 18:25:25 +0000

    Adding examples for Python and CSharp

--- examples/vpc/ts/index.ts
+++ examples/vpc/ts/index.ts
@@ -1,7 +1,6 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as alicloud from "@pulumi/alicloud";
 
-const vpc = new alicloud.vpc.Network("my-vpc", {
+const vpc = new alicloud.vpc.Network("my-ts-vpc", {
     cidrBlock: "10.0.0.0/16",
 });
 
