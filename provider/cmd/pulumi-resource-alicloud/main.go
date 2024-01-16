70c44e1e3ceea89b94737f2eb1585abf36970bf5
Guinevere Saenger <guinevere@pulumi.com>
2024-01-16 10:41:04 -0800

    sort imports

--- provider/cmd/pulumi-resource-alicloud/main.go
+++ provider/cmd/pulumi-resource-alicloud/main.go
@@ -19,9 +19,10 @@ package main
 import (
 	_ "embed"
 
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
+
 	alicloud "github.com/pulumi/pulumi-alicloud/provider/v3"
 	"github.com/pulumi/pulumi-alicloud/provider/v3/pkg/version"
-	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 )
 
 //go:embed schema-embed.json
--- provider/cmd/pulumi-tfgen-alicloud/main.go
+++ provider/cmd/pulumi-tfgen-alicloud/main.go
@@ -15,9 +15,10 @@
 package main
 
 import (
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfgen"
+
 	alicloud "github.com/pulumi/pulumi-alicloud/provider/v3"
 	"github.com/pulumi/pulumi-alicloud/provider/v3/pkg/version"
-	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfgen"
 )
 
 func main() {
